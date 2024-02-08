d6734561b7f3101de5dee789bdfcd374f6bf20f4
Paul Cercueil <paul@crapouillou.net>
2024-02-08 15:49:48 +0100

    aica: Add new API to interface with the audio RAM
    Add the <dc/aram.h> header, and its corresponding source file.
    These files contain a new API that allows to read/write from/to the
    sound RAM, where the source/destination address is from the ARM address
    space.
    
    Add a corresponding aram_addr_t type, which represents a memory address
    within the ARM address space.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ kernel/arch/dreamcast/hardware/aram.c
@@ -0,0 +1,104 @@
+/* KallistiOS ##version##
+
+   aram.c
+   Copyright (C) 2024 Paul Cercueil <paul@crapouillou.net>
+*/
+
+#include <stdint.h>
+#include <dc/g2bus.h>
+#include <dc/aram.h>
+
+static void aram_copy(char *dst, const char *src, size_t size)
+{
+    uint32_t cnt = 0;
+    g2_ctx_t ctx;
+
+    ctx = g2_lock();
+
+    if (!(((uintptr_t)dst | (uintptr_t)src) & 0x3)) {
+        for (; size >= 4; size -= 4) {
+            /* Fifo wait if necessary */
+            if (!(cnt % 8))
+                g2_fifo_wait();
+
+            *(uint32_t *)dst = *(const uint32_t *)src;
+            dst += 4;
+            src += 4;
+            cnt++;
+        }
+    }
+
+    /* Reset FIFO counter */
+    cnt = 0;
+
+    for (; size; size--) {
+        /* Fifo wait if necessary */
+        if (!(cnt % 8))
+            g2_fifo_wait();
+
+        *dst++ = *src++;
+        cnt++;
+    }
+
+    g2_unlock(ctx);
+}
+
+void aram_read(void *dst, aram_addr_t addr, size_t size)
+{
+    const char *src = (const char *)aram_addr_to_host(addr);
+
+    aram_copy((char *)dst, src, size);
+}
+
+void aram_write(aram_addr_t addr, const void *src, size_t size)
+{
+    char *dst = (char *)aram_addr_to_host(addr);
+
+    aram_copy(dst, (const char *)src, size);
+}
+
+static inline uint32_t has_eof(uint32_t dword)
+{
+    uint32_t ret = 0;
+
+    __asm__ __inline__("cmp/str %[ret], %[dword]\n\t"
+                       "movt %[ret]\n\t"
+                       : [ret]"+r"(ret) : [dword]"r"(dword));
+
+    return ret;
+}
+
+char *aram_read_string(aram_addr_t addr, uint32_t *dst, size_t size)
+{
+    const uint32_t *src;
+    uint32_t value, cnt = 0;
+    char *ret = (char *)dst;
+    g2_ctx_t ctx;
+
+    if (addr & 0x3) {
+        ret += 4 - (addr & 0x3);
+        addr &= ~0x3;
+    }
+
+    src = (const uint32_t *)aram_addr_to_host(addr);
+
+    ctx = g2_lock();
+
+    for (; size >= 4; size -= 4) {
+        /* Fifo wait if necessary */
+        if (!(cnt % 8))
+            g2_fifo_wait();
+
+        value = *src++;
+        *dst++ = value;
+
+        if (has_eof(value))
+            break;
+
+        cnt++;
+    }
+
+    g2_unlock(ctx);
+
+    return ret;
+}
