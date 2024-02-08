f44a6e810f57dab6e0d9d7e394d6f1f66473031b
Paul Cercueil <paul@crapouillou.net>
2024-02-08 15:37:58 +0100

    aica: arm: Add header to firmware file
    Add a header to the ARM firmware, which is not used anywhere for now.
    
    Write the address of the header to the last 32-bit word of the sound
    RAM, so that the SH4 can retrieve it.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/sound/arm/aicaos/main.c
+++ kernel/arch/dreamcast/sound/arm/aicaos/main.c
@@ -1,9 +1,42 @@
+#include <cmd_iface.h>
 #include <stddef.h>
 
 extern int main(int argc, char **argv);
 
+extern unsigned char __heap_start, __heap_end;
+
+static char command_buffer[0x10000];
+static char response_buffer[0x10000];
+
+static struct aica_queue command_queue = {
+    .data = (uint32)command_buffer,
+    .size = sizeof(command_buffer),
+    .valid = 1,
+    .process_ok = 1,
+};
+
+static struct aica_queue response_queue = {
+    .data = (uint32)response_buffer,
+    .size = sizeof(response_buffer),
+    .valid = 1,
+    .process_ok = 1,
+};
+
+struct aica_header aica_header = {
+    .cmd_queue = &command_queue,
+    .resp_queue = &response_queue,
+    .channels = (struct aica_channel *)AICA_MEM_CHANNELS,
+    .buffer = &__heap_start,
+};
+
 /* Initialize the OS */
 void arm_main(void)
 {
+    aica_header.buffer_size =
+        (unsigned int)&__heap_end - (unsigned int)&__heap_start;
+
+    /* Set header pointer, so that the SH4 knows where the header is */
+    *(struct aica_header **)AICA_HEADER_ADDR = &aica_header;
+
     main(0, NULL);
 }
