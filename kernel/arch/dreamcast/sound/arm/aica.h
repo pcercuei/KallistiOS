a49e5827d8d23ffb4ec9e4785a6f02d723a48397
Paul Cercueil <paul@crapouillou.net>
2024-02-15 15:22:07 +0100

    aica: Update code to use new register macros
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/sound/arm/aica.h
+++ kernel/arch/dreamcast/sound/arm/aica.h
@@ -3,19 +3,6 @@
 
 #include <stdint.h>
 
-/* volatile unsigned char *dc_snd_base = (unsigned char *)0x00800000; */
-#define dc_snd_base ((volatile uint8_t *)0x00800000)
-
-/* Some convenience macros */
-#define SNDREG32A(x) ((volatile uint32_t *)(dc_snd_base + (x)))
-#define SNDREG32(x) (*SNDREG32A(x))
-#define SNDREG8A(x) (dc_snd_base + (x))
-#define SNDREG8(x) (*SNDREG8A(x))
-#define CHNREG32A(chn, x) SNDREG32A(0x80*(chn) + (x))
-#define CHNREG32(chn, x) (*CHNREG32A(chn, x))
-#define CHNREG8A(chn, x) SNDREG8A(0x80*(chn) + (x))
-#define CHNREG8(chn, x) (*CHNREG8A(chn, x))
-
 /* flags for aica_play() */
 #define AICA_PLAY_DELAY 0x1
 #define AICA_PLAY_LOOP  0x2
