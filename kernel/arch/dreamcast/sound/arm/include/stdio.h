4d4a10628300065611a07cf862dda8ea9aa54987
Paul Cercueil <paul@crapouillou.net>
2024-03-30 11:17:16 +0100

    arm: libc: Add some stuff to stdio.h
    Add some macros and enums we don't actually need, but that allow mikmod
    to compile.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/sound/arm/include/stdio.h
+++ kernel/arch/dreamcast/sound/arm/include/stdio.h
@@ -1,6 +1,16 @@
 #ifndef __AICAOS_STDIO_H
 #define __AICAOS_STDIO_H
 
+typedef int FILE;
+
+enum {
+    SEEK_SET,
+    SEEK_CUR,
+    SEEK_END,
+};
+
+#define EOF ((char)-1)
+
 int aica_printf(const char *restrict fmt, ...);
 #define printf(...) aica_printf(__VA_ARGS__)
 
