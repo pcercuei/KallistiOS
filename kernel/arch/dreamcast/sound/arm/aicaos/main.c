389dbb7509d69b541ca9c0bb1590ecdb5964b75c
Paul Cercueil <paul@crapouillou.net>
2025-03-07 16:46:17 +0100

    aicaos: Clear BSS on startup
    The firmware shouldn't rely on the fact that the sound ram might be
    zeroed. In fact, it actually may not be.
    
    As the BSS area must be cleared before entering the main function, clear
    it in the entry code.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/sound/arm/aicaos/main.c
+++ kernel/arch/dreamcast/sound/arm/aicaos/main.c
@@ -8,11 +8,18 @@
 */
 
 #include <stddef.h>
+#include <string.h>
 
 extern int main(int argc, char **argv);
+
+extern unsigned int __bss_start, __bss_end;
 
 /* Initialize the OS */
 void arm_main(void)
 {
+    /* Clear BSS section */
+    memset(&__bss_start, 0,
+           (unsigned int)&__bss_end - (unsigned int)&__bss_start);
+
     main(0, NULL);
 }
