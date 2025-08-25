95beb9eab0c198bcdb6261e7159e0433da6c7173
Paul Cercueil <paul@crapouillou.net>
2025-08-25 15:11:44 +0200

    CI: Only run build-kos step for supported platforms
    We only need to run the build-kos step for the platforms supported by
    KallistiOS. Add a 'build-kos-check' step which then allows to
    conditionally run the build-kos step.
    
    The .github/build-kos.sh script was already checking for that, but doing
    so before the build-kos step is executed means that we're only
    downloading the Docker image of the toolchain if we will use it.
    
    Also dropped the install of 'coreutils' in build-kos.sh as it's already
    done in the Docker script.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- .github/build-kos.sh
+++ .github/build-kos.sh
@@ -2,11 +2,7 @@
 
 cd /workspace
 
-if [ -d kernel/arch/$1 ] ; then
-	apk --update add --no-cache coreutils
-
-	cp doc/environ.sh.sample environ.sh
-	sed -i "s/KOS_BASE=.*$/KOS_BASE=\\/workspace/" environ.sh
-	. environ.sh
-	make
-fi
+cp doc/environ.sh.sample environ.sh
+sed -i "s/KOS_BASE=.*$/KOS_BASE=\\/workspace/" environ.sh
+. environ.sh
+make
