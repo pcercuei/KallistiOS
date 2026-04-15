e1657a81735d553a5950971acf00278fda1e8c50
Paul Cercueil <paul@crapouillou.net>
2026-04-15 13:35:35 +0200

    ldscripts: Shave off common ldscript bits to common.xc
    A good part of the linker script is composed of arch-independent stuff.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- environ_base.sh
+++ environ_base.sh
@@ -30,7 +30,7 @@ export KOS_INC_PATHS="${KOS_INC_PATHS} -I${KOS_BASE}/include \
 -I${KOS_PORTS}/include"
 
 # "System" libraries.
-export KOS_LIB_PATHS="-L${KOS_BASE}/lib/${KOS_ARCH} -L${KOS_BASE}/addons/lib/${KOS_ARCH} -L${KOS_PORTS}/lib"
+export KOS_LIB_PATHS="-L${KOS_BASE}/lib/${KOS_ARCH} -L${KOS_BASE}/addons/lib/${KOS_ARCH} -L${KOS_BASE}/utils/ldscripts -L${KOS_PORTS}/lib"
 export KOS_LIBS="-Wl,--start-group -lkallisti -lm -lc -lgcc -Wl,--end-group"
 
 # Main arch compiler paths.
